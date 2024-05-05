a5e7716fe3bec250a9d381857d1ba157836a678d
Samuel Kunle <samuelkunle215@gmail.com>
2024-05-05 23:29:30 +0100

    comment added on my running test code

--- LongJavaCourse/CheckingCode.java
+++ LongJavaCourse/CheckingCode.java
@@ -6,8 +6,10 @@ public class CheckingCode {
 }
  class CheckCkech{
     public static void main( String args[]){
-        double cashValue = 100;
+        double cashValue = 1000;
         CheckingCode checkingCode = new CheckingCode(cashValue);
        System.out.println(checkingCode.cashValue);
  }
 }
+
+//Checking my test code, and running test
